3e2d447005cdd33082b203fa5d0eac68e5aea5ad
HackeZ <767110505@qq.com>
2016-08-12 14:03:23 +0800

    cal pruning threhould don

--- Cal_Pruning_Threshold.py
+++ Cal_Pruning_Threshold.py
@@ -1,4 +1,5 @@
 #!usr/bin/python
+from __future__ import division  
 from scipy.spatial.distance import pdist, squareform, euclidean
 from sklearn.cross_validation import KFold
 import numpy as np
@@ -17,37 +18,42 @@ from Cal_Formula import Cal_Class_Contribution as class_contribution
 # Output : W(S) / m  which is KNN-MODEL Pruning Threshold.
 
 def main():
+
 	et_list,feature_hash_table = class_contribution.Cal_Class_Contribution('./knn-data.txt')
 
-	# print et_list
-	# print feature_hash_table
+	data = np.genfromtxt('./knn-data.txt', delimiter=" ", skip_header=False)
+	m = len(data)
 
-	# read data
-    data = np.genfromtxt('./knn-data.txt',  delimiter=" ", skip_header=False)
+	# autosum in Format 5. 
+	# autosum means from c,d = 1,1 to R,n ==> 1/etij
+	autosum = np.sum(et_list)
 
-    m = len(data)
+	w_sx = []
+	i = 0
 
-    w_sx = []
-    i = 0
 	for labels in data:
-		label = labels[-1]
+		label = int(labels[-1])
 		w_x = 0
 		n = len(labels) - 1
-		for j,feature in enumerate(labels[:-1]):
-			# midsum means tij / R * n
-			midsum = feature_hash_table.get("{0} {1}".format(label, j)) / (m * n)
-			# autosum means from i,j to R,n ==> 1/etij
-			autosum = 
-			# rightsum means (1/etij) /autosum
-			rightsum = (1/et_list[i][j]) / autosum
-			w_s.append(1+midsum+rightsum)
-		i++
+
+    	for j,feature in enumerate(labels[:-1]):
+    		# midsum means tij / R * n
+    		midsum = feature_hash_table.get("{0} {1}".format(label, j)) / (m * n)
+    		# rightsum means (1/etij) / autosum
+    		if et_list[i][j] == 0:
+    			continue
+    		else:
+    			rightsum = (1 / et_list[i][j]) / autosum
+    			w_sx.append(1+midsum+rightsum)
+        i+=1
 
 
 	# sum of W(S) / m
 	w_sum = 0
 	for x in w_sx:
 		w_sum += x
+
+	print w_sum / m
 
 	# return W(S)
 	return w_sum / m
